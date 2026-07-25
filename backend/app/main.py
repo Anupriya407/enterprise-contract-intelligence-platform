4a796acdb57b35bfc7b8d398c7cdbdef52323d52
Anupriya Maity <maityanup9892@gmail.com>
2026-07-25 23:47:44 +0530

    feat: add centralized logging

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 from app.api.router import api_router
 from app.core.config import settings
+from app.core.logging import logger
+
+logger.info("Starting ECIP Backend")
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -13,6 +16,8 @@ app.include_router(api_router)
 
 @app.get("/", tags=["Root"])
 def root():
+    logger.info("Root endpoint accessed")
+
     return {
         "message": f"Welcome to {settings.APP_NAME}"
     }
